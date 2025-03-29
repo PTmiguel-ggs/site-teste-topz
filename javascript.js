b9501ffd106285df307263b34eb9b3b4761e751d
Mike. <miguelmoraisaraujo2011@gmail.com>
2025-03-29 15:17:29 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,11 +2,14 @@
 function abrirmenu() {
     let menu = document.getElementById("menu")
     console.log(menu.style.display)
-    if (menu.style.display === "none" || menu.style.display=== "") {
+    if (menu.style.display === "none" || menu.style.display === "") {
         console.log("test")
         menu.style.display = "block"
     }
     else {
         menu.style.display = "none"
     }
+}
+function mostrarMensagem() {
+    alert("Olá");
 }
